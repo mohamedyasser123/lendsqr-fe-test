ce978b392f31459e2d5476542020f55c6300ad1b
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-26 03:40:59 +0300

    feat: implement application routing architecture

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,54 @@
-import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "@/modules/auth/pages/LoginPage";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import AuthLayout from "@/layouts/AuthLayout/AuthLayout";
+import DashboardLayout from "@/layouts/DashboardLayout/DashboardLayout";
+import DashboardPage from "@/modules/dashboard/pages/DashboardPage";
+import Userpage from "@/modules/users/pages/Userpage";
+import UserDetailsPage from "@/modules/users/pages/UserDetailsPage";
+import ProtectedRoute from "./ProtectedRoute";
+import NotFoundPage from "@/components/shared/NotFoundPage/NotFoundPage";
 
-export const router =  createBrowserRouter([]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+
+{
+  element: <ProtectedRoute />,
+  children: [
+    {
+      element: <DashboardLayout />,
+      children: [
+        {
+          path: "/dashboard",
+          element: <DashboardPage />,
+        },
+        {
+          path: "/users",
+          element: <Userpage />,
+        },
+        {
+          path: "/users/:id",
+          element: <UserDetailsPage />,
+        },
+      ],
+    },
+  ],
+},
+
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
